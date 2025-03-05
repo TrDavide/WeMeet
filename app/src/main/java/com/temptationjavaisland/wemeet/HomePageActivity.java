acb77955e6d05080c0d522f680ee50dc55cbaaa1
KevinDex3 <k.dextregonzales@campus.unimib.it>
2025-03-05 17:11:19 +0100

    Homepage e menu

--- app/src/main/java/com/temptationjavaisland/wemeet/HomePageActivity.java
+++ app/src/main/java/com/temptationjavaisland/wemeet/HomePageActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -15,10 +17,6 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
