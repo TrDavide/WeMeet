942c28a5851659634c8c9ddfa75b25be827b1068
KevinDex3 <k.dextregonzales@campus.unimib.it>
2025-03-12 18:18:45 +0100

    Inizo schermata Setting

--- app/src/main/java/com/temptationjavaisland/wemeet/HomePageActivity.java
+++ app/src/main/java/com/temptationjavaisland/wemeet/HomePageActivity.java
@@ -1,14 +1,23 @@
 package com.temptationjavaisland.wemeet;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.temptationjavaisland.wemeet.ui.welcome.fragments.WelcomeFragment;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -17,6 +26,24 @@ public class HomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.fragmentContainerView);
+
+        NavController navController = navHostFragment.getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.locationFragment, R.id.profileFragment, R.id.homepageFragment2, R.id.preferedFragment
+        ).build();
+
+
+        NavigationUI.setupWithNavController(bottomNav, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
     }
 }
