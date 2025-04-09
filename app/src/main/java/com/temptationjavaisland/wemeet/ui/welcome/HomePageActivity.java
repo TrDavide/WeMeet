a9ffed5fe1e71c239380634b0a1f9cee7d93c650
TrDavide <d.trucci@campus.unimib.it>
2025-04-09 17:04:28 +0200

    Home + SearchBar

--- app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/HomePageActivity.java
+++ app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/HomePageActivity.java
@@ -1,22 +1,29 @@
 package com.temptationjavaisland.wemeet.ui.welcome;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.temptationjavaisland.wemeet.R;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private NavController navController;
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +36,16 @@ public class HomePageActivity extends AppCompatActivity {
 
         navController = navHostFragment.getNavController();
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav = findViewById(R.id.bottom_navigation);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.homeFragment, R.id.placeFragment, R.id.preferedFragment, R.id.profileFragment
         ).build();
-
+        bottomNav.setItemActiveIndicatorEnabled(false);
+        bottomNav.setLabelVisibilityMode(BottomNavigationView.LABEL_VISIBILITY_LABELED);
 
         NavigationUI.setupWithNavController(bottomNav, navController);
        // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-
     }
 
 }
--- app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/fragments/LocationFragment.java
+++ app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/fragments/LocationFragment.java
@@ -12,6 +12,10 @@ import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.temptationjavaisland.wemeet.R;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.search.SearchBar;
 
 public class LocationFragment extends Fragment {
 
@@ -38,7 +42,21 @@ public class LocationFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SearchBar searchBar = view.findViewById(R.id.search_bar);
+
+        // Gestione click sul menu (tre puntini)
+        searchBar.setOnMenuItemClickListener(new SearchBar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.action_filter) {
+                    Toast.makeText(getContext(), "Apertura filtri...", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
+
 
 }
