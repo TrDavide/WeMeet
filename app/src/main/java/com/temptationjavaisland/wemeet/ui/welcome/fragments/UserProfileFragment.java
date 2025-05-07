3fafd21105c57c1c7985b4cc1035b5d37fb631e9
MarcoCarpioH <m.carpioherreros@campus.unimib.it>
2025-05-07 12:05:50 +0200

    Creazione UserProfile

---
+++ app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/fragments/UserProfileFragment.java
@@ -0,0 +1,65 @@
+package com.temptationjavaisland.wemeet.ui.welcome.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.search.SearchBar;
+import com.temptationjavaisland.wemeet.R;
+import com.temptationjavaisland.wemeet.adapter.EventRecyclerAdapter;
+import com.temptationjavaisland.wemeet.model.Event;
+import com.temptationjavaisland.wemeet.model.EventAPIResponse;
+import com.temptationjavaisland.wemeet.util.Constants;
+import com.temptationjavaisland.wemeet.util.JSONParserUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+public class UserProfileFragment extends Fragment {
+
+
+    public UserProfileFragment() {}
+
+    public static UserProfileFragment newInstance() {
+        UserProfileFragment fragment = new UserProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {super.onCreate(savedInstanceState);}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerViewUserProfile);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        JSONParserUtils jsonParserUtils = new JSONParserUtils(getContext());
+        try {
+            EventAPIResponse response = jsonParserUtils.parserJSONFileWithGsson(Constants.SAMPLE_JSON_FILENAME);
+            List<Event> eventList= response.getEmbedded().getEvents();
+            EventRecyclerAdapter adapter = new EventRecyclerAdapter(R.layout.event_card, eventList);
+            recyclerView.setAdapter(adapter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SearchBar searchBar = view.findViewById(R.id.search_bar);
+        RecyclerView recyclerViewHome = view.findViewById(R.id.recyclerViewUserProfile);
+        recyclerViewHome.setLayoutManager(new LinearLayoutManager(view.getContext()));
+    }
+}
