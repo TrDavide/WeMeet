c73ba47a6c21879353374dfa20340aed7dca2dc0
TrDavide <d.trucci@campus.unimib.it>
2025-06-22 12:44:32 +0200

    modifiche varie

--- app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/fragments/ParticipantsFragment.java
+++ app/src/main/java/com/temptationjavaisland/wemeet/ui/welcome/fragments/ParticipantsFragment.java
@@ -16,41 +16,15 @@ import com.google.android.material.button.MaterialButton;
 import com.temptationjavaisland.wemeet.R;
 import com.temptationjavaisland.wemeet.model.Event;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ParticipantsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ParticipantsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private Event event;
 
-    public ParticipantsFragment() {
-        // Required empty public constructor
-    }
+    public ParticipantsFragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PartecipantsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ParticipantsFragment newInstance(String param1, String param2) {
+    public static ParticipantsFragment newInstance() {
         ParticipantsFragment fragment = new ParticipantsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,10 +32,6 @@ public class ParticipantsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
